64c8b958fa93771071c6fdc30c1369893a12d308
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-13 12:00:40 +0000

    ランダムな整数値の表示

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
   const result = document.getElementById('result')
 
   btn.addEventListener('click', () => {
-    result.textContent = 'hit!';
+    const n = Math.floor(Math.random() * 3);
+
+    result.textContent = n;
 
   });
 }
