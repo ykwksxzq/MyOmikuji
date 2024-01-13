fef27072a03e2a10726c573ba5847c0781add103
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-13 12:05:37 +0000

    おみくじの表示

--- app.js
+++ app.js
@@ -5,9 +5,19 @@
   const result = document.getElementById('result')
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3);
+    const n = Math.floor(Math.random() * 3)
 
-    result.textContent = n;
+    switch (n) {
+      case 0:
+        result.textContent = '大吉';
+        break;
+      case 1:
+        result.textContent = '中吉';
+        break;
+      case 2:
+        result.textContent = '凶';
+        break;
+    }
 
   });
 }
