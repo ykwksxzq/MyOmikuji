dac214d06a3dc17a40b8f82cd484b0170c63f79c
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-13 12:15:28 +0000

    配列の使用

--- app.js
+++ app.js
@@ -5,19 +5,22 @@
   const result = document.getElementById('result')
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3)
+    const results = ['大吉','中吉','凶','小吉'];
+    const n = Math.floor(Math.random() * results.length)
 
-    switch (n) {
-      case 0:
-        result.textContent = '大吉';
-        break;
-      case 1:
-        result.textContent = '中吉';
-        break;
-      case 2:
-        result.textContent = '凶';
-        break;
-    }
+    result.textContent = results[n];
+
+    // switch (n) {
+    //   case 0:
+    //     result.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     result.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     result.textContent = '凶';
+    //     break;
+    // }
 
   });
 }
